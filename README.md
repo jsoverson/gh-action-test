a1768261c0e6e132dc194759e4e75e7cb473779b
Jarrod Overson <jsoverson@gmail.com>
2023-01-24 14:30:29 -0500

    testing manual release workflow

--- README.md
+++ README.md
@@ -1,1 +1,108 @@
-# gh-action-test
+# Apexlang CLI
+
+The `apex` CLI is a one-stop shop for all projects across all languages.
+
+It's a
+
+- Project templating and scaffolding tool
+- Extensible code generation tool
+- Task runner
+
+For more information, visit [https://apexlang.io](https://apexlang.io).
+
+## Prerequisites
+
+The `apex` CLI depends on Deno.
+
+Install `deno` with instructions
+[here](https://github.com/denoland/deno_install).
+
+## Installation
+
+To install a release version of the `apex` CLI, run the command below:
+
+```
+deno install -A --unstable -f -n apex https://deno.land/x/apex_cli/apex.ts
+```
+
+To install from source, clone this repository and run `./apex run install`
+
+```sh
+git clone https://github.com/apexlang/apex.git
+cd apex
+./apex install # or deno install -A --unstable ./apex.ts
+```
+
+## Usage
+
+Visit [https://apexlang.io](https://apexlang.io) for official documentation and
+usage.
+
+```shell
+apex --help
+```
+
+Output:
+
+```console{title="apex help"}
+  Usage:   apex
+  Version: v0.0.12
+
+  Description:
+
+    A code generation tool using Apexlang, an interface definition language (IDL) for modeling software.
+
+  Options:
+
+    -h, --help     - Show this help.
+    -V, --version  - Show the version number for this program.
+
+  Commands:
+
+    install      <location>          - Install templates locally.
+    new          <template> <dir>    - Create a new project directory using a template.
+    init         <template>          - Initialize a project using a template.
+    generate     [configuration...]  - Run Apexlang generators from a given configuration.
+    list                             - List available resources.
+    describe                         - Describe available resources.
+    watch        [configuration...]  - Watch configuration for changes and trigger code generation.
+    run          [tasks...]          - Run tasks.
+    upgrade                          - Upgrade apex executable to latest or given version.
+    help         [command]           - Show this help or the help of a sub-command.
+    completions                      - Generate shell completions.
+```
+
+## Running tests
+
+To run tests, run the command below:
+
+```sh
+apex test
+```
+
+## Development
+
+To run the development version of the `apex` CLI, use the `apex` script in the
+root of this repository, e.g.
+
+```sh
+./apex help
+```
+
+## Contributing
+
+Please read
+[CONTRIBUTING.md](https://github.com/apexlang/apex/blob/main/CONTRIBUTING.md)
+for details on our code of conduct, and the process for submitting pull requests
+to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available,
+see the [tags on this repository](https://github.com/apexlang/apex/tags).
+
+## License
+
+This project is licensed under the
+[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/) - see the
+[LICENSE](LICENSE) file for details
---
+++ apex.ts
@@ -0,0 +1,103 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-net --allow-run --unstable
+
+import { Command, CompletionsCommand, HelpCommand } from "./src/deps/cliffy.ts";
+import { GithubProvider, UpgradeCommand } from "./src/deps/cliffy.ts";
+import * as log from "https://deno.land/std@0.171.0/log/mod.ts";
+
+const LEVEL =
+  (Deno.env.get("APEX_LOG")?.toUpperCase() as log.LevelName | undefined) ||
+  "INFO";
+
+await setupLogger(LEVEL);
+
+import * as generate from "./src/commands/generate.ts";
+import * as newCmd from "./src/commands/new.ts";
+import * as install from "./src/commands/install.ts";
+import * as init from "./src/commands/init.ts";
+import * as list from "./src/commands/list.ts";
+import * as describe from "./src/commands/describe.ts";
+import * as watch from "./src/commands/watch.ts";
+import * as run from "./src/commands/run.ts";
+import { findApexConfig, setupLogger } from "./src/utils.ts";
+import { parseConfigYaml } from "./src/config.ts";
+
+// Version bump this on release.
+const version = "v0.0.15";
+
+const args = Deno.args;
+
+if (
+  args.length == 1 &&
+  args[0] == "__generate" &&
+  !Deno.isatty(Deno.stdin.rid)
+) {
+  generate.fromStdin();
+} else {
+  const cli = new Command()
+    .default("help")
+    .version(version)
+    .name("apex")
+    .description(
+      "A complete project tool suite based on Apexlang, an interface definition language (IDL) for modeling software.",
+    )
+    .command("install", install.command)
+    .command("new", newCmd.command)
+    .command("init", init.command)
+    .command("generate", generate.command)
+    .command("list", list.command)
+    .command("describe", describe.command)
+    .command("watch", watch.command)
+    .command("run", run.command)
+    .command(
+      "upgrade",
+      new UpgradeCommand({
+        main: "apex.ts",
+        args: [
+          "--allow-read",
+          "--allow-write",
+          "--allow-env",
+          "--allow-net",
+          "--allow-run",
+          "--unstable",
+        ],
+        provider: [new GithubProvider({ repository: "apexlang/apex" })],
+      }),
+    )
+    .command("help", new HelpCommand().global())
+    .command("completions", new CompletionsCommand());
+
+  // Run target if defined in the config.
+  const nonFlagArgs = args.filter((v) => !v.startsWith("-"));
+  if (nonFlagArgs.length > 0 && !cli.getBaseCommand(args[0], true)) {
+    const configPath = findApexConfig();
+    if (!configPath) {
+      log.error("could not find configuration");
+      Deno.exit(1);
+    }
+    let config;
+    try {
+      config = await Deno.readTextFile(configPath);
+    } catch (_e) {
+      log.error(`Could not read config ${configPath}`);
+      Deno.exit(1);
+    }
+    try {
+      const configs = parseConfigYaml(config);
+      for (const cfg of configs) {
+        const taskMap = await run.loadTasks(cfg);
+        await run.runTasks(cfg, taskMap, nonFlagArgs, configs.length == 1);
+      }
+    } catch (e) {
+      log.error(e);
+      Deno.exit(1);
+    }
+    Deno.exit(0);
+  }
+
+  try {
+    await cli.parse(args);
+  } catch (e) {
+    log.error(e);
+    Deno.exit(1);
+  }
+}
