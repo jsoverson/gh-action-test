8f2d1c12344785a13fe7c755d82cfd46d7ee4ac6
Jarrod Overson <jsoverson@gmail.com>
2023-01-24 15:18:24 -0500

    wip

--- .github/workflows/update-readme.ts
+++ .github/workflows/update-readme.ts
@@ -1,4 +1,4 @@
-const readme = "README.md";
+const readme = 'README.md';
 
 console.log(`Updating ${readme} examples`);
 const orig = Deno.readTextFileSync(readme);
@@ -6,16 +6,19 @@ const orig = Deno.readTextFileSync(readme);
 const colorCodes = /\x1b\[\d+;*m/g;
 const upgradeNotification = /\(New version.*$/m;
 
-const apexOutput = await Deno.run({ stdout: "piped", cmd: ["./apex", "help"] })
-  .output();
+const apexOutput = await Deno.run({
+  stdout: 'piped',
+  cmd: ['./apex', 'help'],
+}).output();
 
-const helpText = new TextDecoder().decode(apexOutput)
-  .replace(colorCodes, "")
-  .replace(upgradeNotification, "");
+const helpText = new TextDecoder()
+  .decode(apexOutput)
+  .replace(colorCodes, '')
+  .replace(upgradeNotification, '');
 
 const updated = orig.replace(
   /```(console{title="apex help"}).*?```/s,
-  `\`\`\`$1${helpText}\`\`\``,
+  `\`\`\`$1${helpText}\`\`\``
 );
 
-Deno.writeFileSync(`readme.new.md`, new TextEncoder().encode(updated));
+Deno.writeFileSync(readme, new TextEncoder().encode(updated));
